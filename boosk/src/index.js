a70e4aaf279901a605724bde3c519466bf433eee
Hamdan <105585723+Hamdan-B@users.noreply.github.com>
2024-01-23 14:16:03 +0500

    React Practice
    basics cleared maybe...

--- boosk/src/index.js
+++ boosk/src/index.js
@@ -1,15 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import Book from './components/BookInstance.js';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
 //Practice Code
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 
-let myList = [];
-for(let i = 0; i < 10; i++){
-  myList.push(<Book name="Crazy Book" desc="This book is supposed to be very crazy."/>);
-}
-
-root.render(myList);
+root.render(<p>Some paraa</p>);
