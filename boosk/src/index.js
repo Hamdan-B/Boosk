865d89eba9d26dc8d0c8d0dce42a0d247c7c5393
Hamdan <105585723+Hamdan-B@users.noreply.github.com>
2024-01-23 22:02:23 +0500

    GoogleBook API Test
    Just checking and testing the google book API.

--- boosk/src/index.js
+++ boosk/src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import TemplatePage from "./components/template.js";
+
+// Google Books API key
+const GOOGLE_BOOKS_API = "AIzaSyAbLk6ES7BqijZOLID2YDlKtCcLaQKKt1c";
 
 //Practice Code
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 
-root.render(<p>Some paraa</p>);
+root.render(<TemplatePage />);
