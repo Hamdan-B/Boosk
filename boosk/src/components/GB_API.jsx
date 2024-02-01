454e788207c1db48656fbb8ef192bb1f99eb9f62
Hamdan <105585723+Hamdan-B@users.noreply.github.com>
2024-02-01 21:26:19 +0500

    Practice
    Signals and stuff

--- boosk/src/components/GB_API.jsx
+++ boosk/src/components/GB_API.jsx
@@ -2,6 +2,12 @@ import { signal, effect, computed } from "@preact/signals-react";
 import { useState, useEffect } from "react";
 
 export default function ExamplePage() {
+  const [urlStuff, setUrlStuff] = useState({
+    category: "Fiction",
+    startIndex: 0,
+    maxResults: 20,
+  });
+
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
@@ -13,9 +19,12 @@ export default function ExamplePage() {
 
     const controller = new AbortController();
 
-    fetch("https://www.googleapis.com/books/v1/volumes?q=subject:Fiction", {
-      signal: controller.signal,
-    })
+    fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=subject:${urlStuff.category}&startIndex=${urlStuff.startIndex}&maxResults=${urlStuff.maxResults}`,
+      {
+        signal: controller.signal,
+      }
+    )
       .then((res) => res.json())
       .then((data) => {
         setError(false);
@@ -25,13 +34,21 @@ export default function ExamplePage() {
       .finally(() => setLoading(false));
 
     return () => controller.abort();
-  }, []);
+  }, [urlStuff]);
 
   if (loading) return <p>Loading....</p>;
   if (error) return <p>Error</p>;
 
+  function changeURLStuff() {
+    setUrlStuff({ ...urlStuff, category: "Horror" });
+    console.log(urlStuff);
+  }
+
   return (
     <>
+      <button type="button" onClick={changeURLStuff}>
+        CLICK
+      </button>
       <h1>Books</h1>
       <ul style={{ listStyleType: "none" }}>
         {books.items.map((book) => (
@@ -45,10 +62,7 @@ export default function ExamplePage() {
               width: "12%",
             }}
           >
-            <img
-              src={book.volumeInfo.imageLinks.smallThumbnail}
-              alt="Thumbnail"
-            />
+            <img src={book.volumeInfo.imageLinks.thumbnail} alt="Thumbnail" />
             <div>{book.volumeInfo.title}</div>
             <a
               href={book.volumeInfo.canonicalVolumeLink
